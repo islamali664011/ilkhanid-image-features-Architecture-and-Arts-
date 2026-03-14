b7828409550a3de992ab329cf16cc9dd42a56709
islamali664011 <islamali664011@gmail.com>
2026-03-14 20:03:00 +0200

    Create install_requirements.py

---
+++ install_requirements.py
@@ -0,0 +1,7 @@
+!pip install timm --quiet
+!pip install torchvision --quiet
+!pip install torch --quiet
+!pip install Pillow --quiet
+!pip install numpy --quiet
+!pip install scikit-learn --quiet
+!pip install matplotlib --quiet
